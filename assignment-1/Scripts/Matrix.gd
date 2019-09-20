20d1901b73864c2926030ec35aac778f60f3a610
Forest Anderson <forestkzanderson@gmail.com>
2019-09-20 19:30:34 -0400

    Adding rotation matricies

--- assignment-1/Scripts/Matrix.gd
+++ assignment-1/Scripts/Matrix.gd
@@ -1,5 +1,7 @@
 extends Control
 
+export(String, "translation", "rotate x", "rotate y", "rotate z") var matrix_type
+
 var translation_matrix = [
 	[1, 0, 0, 0],
 	[0, 1, 0, 0],
@@ -7,10 +9,42 @@ var translation_matrix = [
 	["x", "y", "z", 1],
 ]
 
-func _ready():	
+var rotation_x_matrix = [
+	[1, 0, 0, 0],
+	[0, "cos", "sin", 0],
+	[0, "-sin", "cos", 0],
+	[0, 0, 0, 1],
+]
+
+var rotation_y_matrix = [
+	["cos", 0, "-sin", 0],
+	[0, 1, 0, 0],
+	["sin", 0, "cos", 0],
+	[0, 0, 0, 1],
+]
+
+var rotation_z_matrix = [
+	["cos", "sin", 0, 0],
+	["-sin", "cos", 0, 0],
+	[0, 0, 1, 0],
+	[0, 0, 0, 1],
+]
+
+func _ready():
+	
+	var matrix_used
+	
+	if matrix_type == "translation":
+		matrix_used = translation_matrix
+	elif matrix_type == "rotate x":
+		matrix_used = rotation_x_matrix
+	elif matrix_type == "rotate y":
+		matrix_used = rotation_y_matrix
+	elif matrix_type == "rotate z":
+		matrix_used = rotation_z_matrix
 	
 	var element_count = 0
-	for row in translation_matrix:
+	for row in matrix_used:
 		for element in row:
 			var matrix_element = $GridContainer.get_child(element_count)
 			element_count += 1
